a0fa308af40afc3b697afb54d2d49acfb5cc1007
xavier-de-oliveira <xavier.de-oliveira@lycee-de-beauvoir.education>
2025-12-09 15:51:03 +0100

    Création module Logs et branche dev

---
+++ Logs/log.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xavier.d on 09/12/2025.
+//
+
+#include "log.h"
---
+++ Logs/log.h
@@ -0,0 +1,8 @@
+//
+// Created by xavier.d on 09/12/2025.
+//
+
+#ifndef INDEX_HTML_LOG_H
+#define INDEX_HTML_LOG_H
+
+#endif //INDEX_HTML_LOG_H
